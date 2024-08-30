8d7fb2fdc7abe9824d9ad9a5781117d63f276650
Ramil Zaripov <zarram89@gmail.com>
2024-08-30 12:30:48 +0500

    1.7 Разобьёт код на модули

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import NewTaskButtonView from './view/new-task-button-view';
+import FilterView from './view/filter-view';
 import { render } from './render';
+
 
 const siteMainElement = document.querySelector('.main');
 const siteHeaderElement = siteMainElement.querySelector('.main__control');
 
 render(new NewTaskButtonView(), siteHeaderElement);
+render(new FilterView(), siteMainElement);
