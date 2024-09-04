1b9d411ba4c871d7c8c4ca878f21dee12f11bbb9
Ramil Zaripov <zarram89@gmail.com>
2024-09-04 09:18:26 +0500

    1.8 Добавит презентер доски

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import NewTaskButtonView from './view/new-task-button-view';
 import FilterView from './view/filter-view';
 import { render } from './render';
+import BoardPresenter from './presenter/board-presenter';
 
 
 const siteMainElement = document.querySelector('.main');
 const siteHeaderElement = siteMainElement.querySelector('.main__control');
+const boardPresenter = new BoardPresenter({boardContainer: siteMainElement});
 
 render(new NewTaskButtonView(), siteHeaderElement);
 render(new FilterView(), siteMainElement);
+
+boardPresenter.init();
