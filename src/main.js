d865c40d591a00ac0a54ef6f41cefc1da483b5e9
Ramil Zaripov <zarram89@gmail.com>
2024-09-04 12:43:58 +0500

    2.1 Добавит моковые данные

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import FilterView from './view/filter-view';
 import { render } from './render';
 import BoardPresenter from './presenter/board-presenter';
 
-
 const siteMainElement = document.querySelector('.main');
 const siteHeaderElement = siteMainElement.querySelector('.main__control');
 const boardPresenter = new BoardPresenter({boardContainer: siteMainElement});
@@ -12,3 +11,4 @@ render(new NewTaskButtonView(), siteHeaderElement);
 render(new FilterView(), siteMainElement);
 
 boardPresenter.init();
+
